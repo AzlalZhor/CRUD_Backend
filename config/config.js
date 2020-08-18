a64698a0884aa3f9c9e3f2c6d6f7a1de5c6e2d98
Azlal_Zhor <azlal.zhoraz@gmail.com>
2020-08-18 11:47:07 +0100

    init project

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    port_http: 8080,
+    server : '127.0.0.1',
+    port : 3306,
+    database : 'produit_bd',
+    username : 'root',
+    password : '',
+    dialect : 'mysql',
+};
